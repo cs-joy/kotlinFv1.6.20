c82b6b9544c2f9d98bc4aad74abf0e62a23dfb32
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-04-11 07:47:47 +0600

    Update Functions.kt
    - `Unit` return type

--- docs/Basic_Syntax/Functions.kt
+++ docs/Basic_Syntax/Functions.kt
@@ -5,10 +5,27 @@ fun sum(a: Int, b: Int): Int {
 //A function body can be an expression. Its return type is inferred
 fun add(a: Int, b: Int) = a + b
 
+//A function that returns no meaningful value.
+fun printSum(x: Int, y:Int): Unit {
+    println("sum of $x and $y is:>_ ${x + y}")
+}
+
+//Unit return type can be omitted
+fun printSJ(s: Int, j:Int) {
+    println("sum of $s and $j is:>_ ${s + j}")
+}
+
+//main function
 fun main() {
     print("sum of 3 and 5 is ")
     println(sum(3, 5))
 
     //inferred
     println("19 + 23 is ${sum(19, 23)}")
+
+    //returns no meaningfull value
+    printSum(20,2)
+
+    //omitted
+    printSJ(1,1)
 }
