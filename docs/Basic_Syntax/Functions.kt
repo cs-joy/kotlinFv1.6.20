672b4f4ff87eaad08c289ec6db89186d4dca1824
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-04-11 07:30:08 +0600

    adding inferred type function

--- docs/Basic_Syntax/Functions.kt
+++ docs/Basic_Syntax/Functions.kt
@@ -2,7 +2,13 @@ fun sum(a: Int, b: Int): Int {
     return a + b
 }
 
+//A function body can be an expression. Its return type is inferred
+fun add(a: Int, b: Int) = a + b
+
 fun main() {
     print("sum of 3 and 5 is ")
     println(sum(3, 5))
+
+    //inferred
+    println("19 + 23 is ${sum(19, 23)}")
 }
